ea94c126fa3953934381b4bcf871c91ece0dfd21
KSIDDHARDHA <siddhardhasiddhu46@gmail.com>
2023-04-25 19:41:35 +0530

    mini project

---
+++ client/src/main.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import { TransactionProvider } from "./context/TransactionContext";
+import "./index.css";
+
+ReactDOM.render(
+  <TransactionProvider>
+    <App />
+  </TransactionProvider>,
+  document.getElementById("root"),
+);
